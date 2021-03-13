d0c2ad412e5d8b2e6eb090e5c895efedefc20687
el13-1 <sigitova.lena@yandex.ru>
2021-03-13 20:40:15 +0300

    add section group

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swip', {
+  // Optional parameters
+  
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  
+});
+
+const swiperGroop = new Swiper('.swip-gr', {
   // Optional parameters
   
   loop: true,
